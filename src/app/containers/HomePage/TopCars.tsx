8c61a315457f5b78884998f9ee67649c960680cd
APdrone <amanpatwalfor@gmail.com>
2022-04-16 10:17:21 +0530

    updating config files

--- src/app/containers/HomePage/TopCars.tsx
+++ src/app/containers/HomePage/TopCars.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
-import { ICar } from "../../../typings/car";
+// import { ICar } from "../../../typings/car";
 import Car from "../../components/car";
 import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
@@ -101,30 +101,31 @@ const TopCars = () => {
     setLoading(false);
   };
 
-  const testCar: ICar = {
-    name: "Audi S3 Car",
-    mileage: "10k",
-    thumbnailSrc:
-      "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
-    dailyPrice: 70,
-    monthlyPrice: 1600,
-    gearType: "Auto",
-    gas: "Petrol",
-  };
+  // const testCar: ICar = {
+  //   name: "Audi S3 Car",
+  //   mileage: "10k",
+  //   thumbnailSrc:
+  //     "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+  //   dailyPrice: 70,
+  //   monthlyPrice: 1600,
+  //   gearType: "Auto",
+  //   gas: "Petrol",
+  // };
 
-  const testCar2: ICar = {
-    name: "HONDA cITY 5 Seater Car",
-    mileage: "20k",
-    thumbnailSrc:
-      "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
-    dailyPrice: 50,
-    monthlyPrice: 1500,
-    gearType: "Auto",
-    gas: "Petrol",
-  };
+  // const testCar2: ICar = {
+  //   name: "HONDA cITY 5 Seater Car",
+  //   mileage: "20k",
+  //   thumbnailSrc:
+  //     "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+  //   dailyPrice: 50,
+  //   monthlyPrice: 1500,
+  //   gearType: "Auto",
+  //   gas: "Petrol",
+  // };
 
   useEffect(() => {
     fetchTopCars();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const isEmptyTopCars = !topCars || topCars.length === 0;
